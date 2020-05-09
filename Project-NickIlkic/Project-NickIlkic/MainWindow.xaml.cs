e00f354972f04b7358fadaefe61b60584d3bfacc
Nick <nickilkic@gmail.com>
2020-05-09 14:41:18 -0400

    final

---
+++ Project-NickIlkic/Project-NickIlkic/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project_NickIlkic
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        LibraryEntities5 libDB = new LibraryEntities5();
+        Service.Service s = new Service.Service();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            txtTitle.Text = String.Empty;
+            txtAuthor.Text = String.Empty;
+            txtISDN.Text = String.Empty;
+            cbBooks.SelectedItem = cbBooks.Items[0];
+            cbUsers.SelectedItem = cbBooks.Items[0];
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string title = txtTitle.Text;
+            string author = txtAuthor.Text;
+            string isdn =  txtISDN.Text;
+
+            Book b = new Book() { Title = title, Author = author, ISDN = isdn };
+
+            s.AddBook(b);
+
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var query = from book in libDB.Books
+                        select new { book.Id, book.Title, book.Author, book.ISDN };
+
+            BooksDataGrid.ItemsSource = query.ToList();
+            cbBooks.ItemsSource = query.ToList();
+
+            var VWBorrowed = from user in libDB.VWBorroweds
+                        select new { user.FirstName, user.LastName, user.Title };
+ 
+
+            UsersDataGrid.ItemsSource = VWBorrowed.ToList();
+            
+
+            var comboBox = from user in libDB.Users
+                         select new { user.FirstName, user.LastName };
+
+            cbUsers.ItemsSource = comboBox.ToList();
+        }
+
+        private void BooksDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void BooksDataGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            object item = BooksDataGrid.SelectedItem;
+            string ID = (BooksDataGrid.SelectedCells[0].Column.GetCellContent(item) as TextBlock).Text;
+            txtTitle.Text = ID;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            int bookId = cbBooks.SelectedIndex + 1;
+            int userId = cbUsers.SelectedIndex + 1;
+            Console.WriteLine(bookId + " " + userId);
+            s.Lendbook(bookId, userId);
+        }
+    }
+}
---
+++ Project-NickIlkic/Project-NickIlkic/Service/Service.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_NickIlkic.Service
+{
+    class Service
+    {
+        LibraryEntities5 libDB = new LibraryEntities5();
+        
+
+        public int AddBook(Book book)
+        {
+            using (var bookContext = new LibraryEntities5())
+            {
+                bookContext.Books.Add(book);
+                bookContext.SaveChanges();
+            }
+            var query = from b in libDB.Books
+                        where b.ISDN == "41242"
+                        select b.Id;
+
+            return query.FirstOrDefault();
+
+        }
+
+
+        public bool EditBook(int bookId, string newName)
+        {
+            using (var bookContext = new LibraryEntities5())
+            {
+
+                var query = from b in bookContext.Books
+                            where b.Id == bookId
+                            select b;
+
+                Book selectedOne = query.First();
+                selectedOne.Title = newName;
+
+
+                bookContext.SaveChanges();
+
+            }
+            return true;
+        }
+
+        public bool Lendbook(int bookId, int userId)
+        {
+            using (var bookContext = new LibraryEntities5())
+            {
+
+                var query = from u in bookContext.Users
+                            where u.Id == userId
+                            select u;
+
+                User selectedOne = query.First();
+                selectedOne.BorrowedBooks = bookId;
+
+
+                bookContext.SaveChanges();
+
+            }
+            return true;
+        }
+
+    }
+}
